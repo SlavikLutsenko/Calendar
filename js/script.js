666fc31d55e8c9ce3e30e3a25a09253254954f76
Slavik <slavka.com.ua@gmail.com>
2015-03-03 20:23:22 +0200

    v3
    add new font
    transformed into a widget (need to finish)
    add hovers
    add selected date

--- js/script.js
+++ js/script.js
@@ -40,43 +40,97 @@ var months = new Array(
 
 var calendar = document.querySelector(".b-calendar"),
 	today = new Date(),
-	inputDate = calendar.querySelector("input.today"),
-	controls = calendar.querySelector(".calendar .b-controls"),
-	table = calendar.querySelector("table");
+    currentData = new Date(),
+    selectDate = new Date(),
+    table = null,
+	inputDate = document.querySelector("input.calendar"),
+	controls = calendar.querySelector(".b-controls"),
+    controls_date = controls.querySelector(".date"),
+    goToday = calendar.querySelector(".goToday");
 
-inputDate.value = months[today.getMonth()].name + " " + today.getDate();
-controls.querySelector(".date").innerText = months[today.getMonth()].name + " " + today.getFullYear();
+selectDate.setDate(13);
+inputDate.value = months[selectDate.getMonth()].name + " " + selectDate.getDate();
+/*
+inputDate.addEventListener("focus", function(){
+	calendar.style.display = "block";
+}, false);
 
+inputDate.addEventListener("blur", function(){
+	calendar.style.display = "none";
+}, false);
+*/
 function ShowCalendar (array) {
-	var tableRow, tableCell;
+    if(table != null)
+        calendar.removeChild(table);
+	table = document.createElement("table");
+	var	tableRow, tableCell, flag = true;
 	for (var i = 0; i < array.length; i++) {
-		tableRow = document.createElement("tr")
+		tableRow = document.createElement("tr");
 		for (var j = 0; j < array[i].length; j++) {
-			tableCell = document.createElement("td");
+			tableCell = document.createElement(i == 0 ? "th" : "td");
 			tableCell.innerText = array[i][j];
+			if(array[i][j] == 1) flag = !flag;
+			if(flag && i != 0) tableCell.className = "other_month";
+            if(today.toString() == currentData.toString() && array[i][j] == today.getDate() && !flag) 
+                tableCell.className += " today";
+            if(selectDate.getMonth() == currentData.getMonth() && selectDate.getFullYear() == currentData.getFullYear() && array[i][j] == selectDate.getDate() && !flag) 
+                tableCell.className += " select";
 			tableRow.appendChild(tableCell);
 		};
 		table.appendChild(tableRow);
 	};
-	
+	table.className = "calendar";
+	calendar.insertBefore(table, calendar.lastElementChild);
 }
 
-function ShowMonth (id_month, year) {
-	var days = new Array(7),
-		date,
-		firstDate = (new Date(months[id_month].name + " " + year)).getDay();
-	days[0] = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
-	for (var i = 1; i < days.length; i++) {
-		days[i] = new Array(7);
-	};
-	date = new Date((months[id_month - 1].day - (firstDate == 0 ? 6 : firstDate)) + " " + months[id_month - 1].name + " " + year);
-	for (var i = 1; i < days.length; i++) {
-		for (var j = 0; j < days[i].length; j++) {
-			days[i][j] = date.getDate();
-			date.setDate(date.getDate() + 1);
-		};
-	};
-	ShowCalendar(days);
+function ShowMonth (date) {
+    controls_date.innerText = months[date.getMonth()].name + " " + date.getFullYear();
+    var days = new Array(7),
+        date = new Date(months[date.getMonth()].name + " " + date.getFullYear());
+    days[0] = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
+    for (var i = 1; i < days.length; i++)
+        days[i] = new Array(7);
+    for (var i = 0, end = date.getDay() == 0 ? 7 : date.getDay(); i < end; i++)
+        date.setDate(date.getDate() - 1);
+    for (var i = 1; i < days.length; i++) {
+        for (var j = 0; j < days[i].length; j++) {
+            days[i][j] = date.getDate();
+            date.setDate(date.getDate() + 1);
+        };
+    };
+    ShowCalendar(days);
 }
 
-ShowMonth(today.getMonth(), today.getFullYear());
+ShowMonth(currentData);
+
+controls.addEventListener("click", function (e) {
+    var el = e.target;
+    if(el.className.search("left") != -1){
+        currentData.setMonth(currentData.getMonth() - 1);
+        ShowMonth(currentData);
+    }
+    if(el.className.search("right") != -1){
+        currentData.setMonth(currentData.getMonth() + 1);
+        ShowMonth(currentData);
+    }
+}, false);
+
+goToday.addEventListener("click", function(){
+    selectDate = new Date();
+    currentData = new Date();
+    ShowMonth(currentData);
+}, false);
+
+calendar.addEventListener("click", function (e) {
+    var el = e.target;
+    if(el.tagName.toLowerCase() == "td"){
+        var previousSelect = calendar.querySelector("td.select");
+        if(previousSelect)
+            previousSelect.className = previousSelect.className.replace("select", "");
+        el.className += " select";
+        selectDate.setDate(el.innerText);
+        selectDate.setMonth(currentData.getMonth());
+        selectDate.setFullYear(currentData.getFullYear());
+        inputDate.value = months[selectDate.getMonth()].name + " " + selectDate.getDate();
+    }
+}, false);
